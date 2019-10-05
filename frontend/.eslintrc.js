4cd0094dd2b409c0cd189e7d301dbd5f85748683
andee <andrey14501450@gmail.com>
2019-10-05 17:36:57 +0300

    Added eslint with configuration

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["airbnb", "prettier", "prettier/react", "plugin:you-dont-need-lodash-underscore/compatible"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
