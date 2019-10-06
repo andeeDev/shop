db9b99b47b50932f2eed8ff811b82779597627b8
andee <andeevacik@gmail.com>
2019-10-06 16:51:23 +0300

    feat: Created product model

---
+++ backend/app/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function categories()
+    {
+        return $this->belongsTo(
+            Category::class,
+            'ctg_id',
+            'id'
+            );
+    }
+}
