84d8bca3e8e85e67f6b33f3cdc0c103ecc8d30d3
andee <andeevacik@gmail.com>
2019-10-06 17:47:15 +0300

    feat: Added user model

---
+++ backend/app/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+    public function orders()
+    {
+        return $this->hasMany(\App\Order::class,
+            'user_id',
+            'id');
+    }
+}
