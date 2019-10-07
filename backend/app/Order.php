9963063b6e4a9e726ab4db448d249f9ad8811c7c
andee <andeevacik@gmail.com>
2019-10-07 10:02:46 +0300

    fix: edit models to unable display date

--- backend/app/Order.php
+++ backend/app/Order.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $hidden = ['date'];
+
+
     public function products()
     {
         return $this->belongsToMany(
